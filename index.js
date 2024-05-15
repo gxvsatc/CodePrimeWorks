86db4613d6dc1bf17dbfdcb414565dbc8a92f11f
gxvsatc <gs1baaldicu@hotmail.com>
2024-05-16 02:09:45 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-function rotateRight(head, k) {
-  if (!head) return null;
-  let length = 1;
-  let tail = head;
-  while (tail.next) {
-    length++;
-    tail = tail.next;
-  }
-  k %= length;
-  if (k === 0) return head;
-  let newTail = head;
-  for (let i = 0; i < length - k - 1; i++) {
-    newTail = newTail.next;
-  }
-  const newHead = newTail.next;
-  newTail.next = null;
-  tail.next = head;
-  return newHead;
-}
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
